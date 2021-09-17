6ff893bbed7c0a5840c4cd8dbf81898ba1a27140
unknown <vasilevski12@mail.ru>
2021-09-18 00:57:33 +0300

    coupon server snly

--- src/main/java/com/jb/projectNo2/Controller/LoginController.java
+++ src/main/java/com/jb/projectNo2/Controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 
-@RestController
+//@RestController
 @RequestMapping("login")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
