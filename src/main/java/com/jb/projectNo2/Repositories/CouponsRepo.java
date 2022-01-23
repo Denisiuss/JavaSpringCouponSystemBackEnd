fedb14091de578b7f66a4aba524d4b2c1d94218c
Denis Vasilevsky <you@example.com>
2022-01-23 09:28:42 +0200

    make it better

--- src/main/java/com/jb/projectNo2/Repositories/CouponsRepo.java
+++ src/main/java/com/jb/projectNo2/Repositories/CouponsRepo.java
@@ -27,20 +27,20 @@ public interface CouponsRepo extends JpaRepository<Coupons, Long> {
     void addCouponPurchase(@Param("customerId") long customerId, @Param("couponId") long couponId);
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value = "SELECT * FROM coupons WHERE ID IN (SELECT COUPON_ID FROM couponsno2.customers_vs_coupons WHERE CUSTOMER_ID = :customerId)", nativeQuery = true)
+    @Query(value = "SELECT * FROM coupons WHERE ID IN (SELECT COUPON_ID FROM customers_vs_coupons WHERE CUSTOMER_ID = :customerId)", nativeQuery = true)
     ArrayList<Coupons> findByCustomerId (@Param("customerId") long customerId);
     ArrayList<Coupons> findByCategories(Categories categories);
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value = "SELECT * FROM couponsno2.coupons WHERE ID IN (SELECT COUPON_ID FROM couponsno2.customers_vs_coupons WHERE CUSTOMER_ID = :customerId) AND CATEGORIES = :categories", nativeQuery = true)
+    @Query(value = "SELECT * FROM coupons WHERE ID IN (SELECT COUPON_ID FROM customers_vs_coupons WHERE CUSTOMER_ID = :customerId) AND CATEGORIES = :categories", nativeQuery = true)
     ArrayList<Coupons> findCustomerCouponsByCategory(@Param("customerId") long customerId, @Param("categories") String categories);
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query (value = "SELECT * FROM couponsno2.coupons WHERE ID IN (SELECT COUPON_ID FROM couponsno2.customers_vs_coupons WHERE CUSTOMER_ID = :customerId) AND PRICE<= :maxPrice", nativeQuery = true)
+    @Query (value = "SELECT * FROM coupons WHERE ID IN (SELECT COUPON_ID FROM customers_vs_coupons WHERE CUSTOMER_ID = :customerId) AND PRICE<= :maxPrice", nativeQuery = true)
     ArrayList<Coupons> findByCustomerIdAndMaxPrice (@Param("customerId") long customerId,@Param("maxPrice") double maxPrice);
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query (value = "DELETE FROM couponsno2.coupons WHERE END_DATE< NOW()"
+    @Query (value = "DELETE FROM coupons WHERE END_DATE< NOW()"
             //"Delete from couponsno2.coupons WHERE end_date<=CURRENT_DATE"
             , nativeQuery = true)
     void deleteByEndDate(@Param("now") Date now);
