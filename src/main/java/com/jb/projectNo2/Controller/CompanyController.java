86a23208e61e671570cd28d7f4d39198f3de4822
unknown <vasilevski12@mail.ru>
2022-01-25 05:23:01 +0200

    make it better

--- src/main/java/com/jb/projectNo2/Controller/CompanyController.java
+++ src/main/java/com/jb/projectNo2/Controller/CompanyController.java
@@ -4,13 +4,16 @@ import com.jb.projectNo2.Beans.Categories;
 import com.jb.projectNo2.Beans.Companies;
 import com.jb.projectNo2.Beans.Coupons;
 import com.jb.projectNo2.Beans.UserDetails;
+import com.jb.projectNo2.Exceptions.CouponException;
 import com.jb.projectNo2.Login.ClientType;
 import com.jb.projectNo2.Login.LoginManager;
 import com.jb.projectNo2.Services.AdminService;
 import com.jb.projectNo2.Services.CompanyService;
 import com.jb.projectNo2.Utils.JWTutil;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,10 +27,22 @@ import java.sql.SQLException;
 public class CompanyController {
     CompanyService companyService;
     @Autowired
-    private JWTutil jwTutil;
+    private JWTutil jwtUtil;
     @Autowired
     private LoginManager loginManager;
     private long companyId;
+
+    private HttpHeaders getHeaders(String token){
+        //create new userDetail and DI
+        UserDetails userDetails = new UserDetails();
+        userDetails.setEmail(jwtUtil.extractEmail(token));
+        userDetails.setUserType((String)jwtUtil.extractAllClaims(token).get("userType"));
+        userDetails.setId((Long) jwtUtil.extractAllClaims(token).get("userId"));
+        //send ok with header of new token
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization",jwtUtil.generateToken(userDetails));
+        return httpHeaders;
+    }
 
     @PostMapping("Login")
     private ResponseEntity<?> userLogin (@RequestBody UserDetails userDetails) throws SQLException, InterruptedException {
@@ -35,51 +50,117 @@ public class CompanyController {
             companyService = (CompanyService) loginManager.login(userDetails.getEmail(), userDetails.getPassword(), ClientType.Company);
             if (companyService != null) {
                 userDetails.setId(companyId=companyService.getCompanyId(userDetails.getEmail()));
-                String token = jwTutil.generateToken(userDetails);
-                return ResponseEntity.accepted().header(jwTutil.extractEmail(token)).body(token);
+                String token = jwtUtil.generateToken(userDetails);
+                return ResponseEntity.accepted().header(jwtUtil.extractEmail(token)).body(token);
             }
         }
         return new ResponseEntity<>("Incorrect login", HttpStatus.UNAUTHORIZED);
     }
 
-
+    @PostMapping("addCoupon")
+    public ResponseEntity<?> addCoupon(@RequestHeader(name = "Authorization") String token, @RequestBody Coupons coupons) throws MalformedJwtException, CouponException {
+        if (jwtUtil.validateToken(token)) {
+            companyService.addCoupon(coupons);
+        }
+        return ResponseEntity.ok().headers(getHeaders(token)).body("Coupon was added");
+    }
+    /*
     @PostMapping("addCoupon")
     @ResponseStatus(HttpStatus.CREATED)
     public void addCoupon(@RequestBody Coupons coupons){companyService.addCoupon(coupons);}
+    */
 
+    @PostMapping("updateCoupon")
+    public ResponseEntity<?> updateCoupon(@RequestHeader(name = "Authorization") String token, @RequestBody Coupons coupons) throws MalformedJwtException, CouponException {
+        if (jwtUtil.validateToken(token)) {
+            companyService.updateCoupon(coupons);
+        }
+        return ResponseEntity.ok().headers(getHeaders(token)).body("Customer has been updated!");
+    }
+    /*
     @PostMapping("updateCoupon")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void updateCoupon(@RequestBody Coupons coupons) {companyService.updateCoupon(coupons);}
+    */
 
+    @DeleteMapping("deleteCoupon/{couponId}")
+    public ResponseEntity<?> deleteCoupon(@RequestHeader(name = "Authorization") String token, @PathVariable long couponId) throws MalformedJwtException, CouponException {
+        if (jwtUtil.validateToken(token)) {
+            companyService.deleteCoupon(couponId);
+        }
+        return ResponseEntity.ok().headers(getHeaders(token)).body("Coupon was deleted Successfully");
+    }
+    /*
     @DeleteMapping("deleteCoupon/{couponId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteCoupon(@PathVariable long couponId) {companyService.deleteCoupon(couponId);}
+    */
 
+    @GetMapping("getCoupons")
+    public ResponseEntity<?> getAllCompanyCoupons (@RequestHeader(name = "Authorization") String token) throws MalformedJwtException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(companyService.getCompanyCoupons());
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     @GetMapping("getCoupons")
     public ResponseEntity<?> getCompanyCoupons () {
         return new ResponseEntity<>(companyService.getCompanyCoupons(), HttpStatus.ACCEPTED);
-    }
+    }*/
 
+    @GetMapping("getOneCouponById/{id}")
+    public ResponseEntity<?> getOneCouponById(@RequestHeader(name = "Authorization") String token, @PathVariable long id) throws MalformedJwtException, CouponException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(companyService.getCompanyCouponById(id));
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     @GetMapping("getOneCouponById/{id}")
     public ResponseEntity<?> getOneCouponById(@PathVariable long id){
         return new ResponseEntity<>(companyService.getCompanyCouponById(id), HttpStatus.ACCEPTED);
-    }
+    }*/
 
+    @GetMapping("findByCategories/{categories}")
+    public ResponseEntity<?> getCompanyCouponsByCategory(@RequestHeader(name = "Authorization") String token, @PathVariable("categories") Categories categories) throws MalformedJwtException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(companyService.getCompanyCouponsByCategory(categories));
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     @GetMapping("findByCategories/{categories}")
     public ResponseEntity<?> getCompanyCouponsByCategory(@PathVariable("categories") Categories categories) {
         return new ResponseEntity<>(companyService.getCompanyCouponsByCategory(categories), HttpStatus.ACCEPTED);
-    }
+    }*/
 
+    @GetMapping("coupons/{maxPrice}")
+    public ResponseEntity<?> getCouponsByMaxPrice(@RequestHeader(name = "Authorization") String token, @PathVariable double maxPrice) throws MalformedJwtException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(companyService.getCompanyCouponsByMaxPrice(maxPrice));
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     @GetMapping("coupons/{maxPrice}")
     public ResponseEntity<?> getCouponsByMaxPrice(@PathVariable double maxPrice){
         return new ResponseEntity<>(companyService.getCompanyCouponsByMaxPrice(maxPrice), HttpStatus.ACCEPTED);
-    }
+    }*/
 
+    @GetMapping("getDetails")
+    public ResponseEntity<?> getDetails(@RequestHeader(name = "Authorization") String token) throws MalformedJwtException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(companyService.getCompanyDetails());
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     @GetMapping("getDetails")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<?> getDetails(){
         return new ResponseEntity<>(companyService.getCompanyDetails(), HttpStatus.ACCEPTED);
-    }
+    }*/
 
 
 }
--- src/main/java/com/jb/projectNo2/Controller/CustomerController.java
+++ src/main/java/com/jb/projectNo2/Controller/CustomerController.java
@@ -9,8 +9,10 @@ import com.jb.projectNo2.Login.ClientType;
 import com.jb.projectNo2.Login.LoginManager;
 import com.jb.projectNo2.Services.CustomerService;
 import com.jb.projectNo2.Utils.JWTutil;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,10 +28,22 @@ import java.util.stream.Collectors;
 public class CustomerController {
     CustomerService customerService;
     @Autowired
-    private JWTutil jwTutil;
+    private JWTutil jwtUtil;
     @Autowired
     private LoginManager loginManager;
     private long customerId;
+
+    private HttpHeaders getHeaders(String token){
+        //create new userDetail and DI
+        UserDetails userDetails = new UserDetails();
+        userDetails.setEmail(jwtUtil.extractEmail(token));
+        userDetails.setUserType((String)jwtUtil.extractAllClaims(token).get("userType"));
+        userDetails.setId((Long) jwtUtil.extractAllClaims(token).get("userId"));
+        //send ok with header of new token
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization",jwtUtil.generateToken(userDetails));
+        return httpHeaders;
+    }
 
     @PostMapping("Login")
     private ResponseEntity<?> userLogin (@RequestBody UserDetails userDetails) throws SQLException, InterruptedException {
@@ -37,13 +51,21 @@ public class CustomerController {
             customerService = (CustomerService) loginManager.login(userDetails.getEmail(), userDetails.getPassword(), ClientType.Customer);
             if (customerService != null) {
                 userDetails.setId(customerId=customerService.getCustomerId(userDetails.getEmail(), userDetails.getPassword()));
-                String token = jwTutil.generateToken(userDetails);
-                return ResponseEntity.accepted().header(jwTutil.extractEmail(token)).body(token);
+                String token = jwtUtil.generateToken(userDetails);
+                return ResponseEntity.accepted().header(jwtUtil.extractEmail(token)).body(token);
             }
         }
         return new ResponseEntity<>("Incorrect login", HttpStatus.UNAUTHORIZED);
     }
 
+    @PostMapping("purchaseCoupon")
+    public ResponseEntity<?> purchaseCoupon(@RequestHeader(name = "Authorization") String token, @RequestBody Coupons coupons) throws MalformedJwtException, CouponException {
+        if (jwtUtil.validateToken(token)) {
+            customerService.purchaseCoupon(coupons);
+        }
+        return ResponseEntity.ok().headers(getHeaders(token)).body("Coupon "+coupons.getTitle()+" purchased");
+    }
+    /*
     @PostMapping("purchaseCoupon")
     private ResponseEntity<?>purchaseCoupon(@RequestBody Coupons coupons) throws CouponException {
         try {
@@ -52,25 +74,41 @@ public class CustomerController {
         }catch (CouponException e){
             throw new CouponException(e.getMessage());
         }
-    }
+    }*/
 
+    @GetMapping("myCoupons/{maxPrice}")
+    public ResponseEntity<?> getCouponsByMaxPrice(@RequestHeader(name = "Authorization") String token, @PathVariable double maxPrice) throws MalformedJwtException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(customerService.getCustomerCouponsByMaxPrice(maxPrice));
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     /**
      * This is a getter method by customer id and coupons max price.
      * @param maxPrice of the coupons
      * @return ArrayList of all the coupons that 1 customer bought by customer id number and max price of the coupon.
      * @throws CustomerUserException
-     */
+     *
     @GetMapping("myCoupons/{maxPrice}")
     public ResponseEntity<?> getCouponsByMaxPrice(@PathVariable double maxPrice){
         return new ResponseEntity<>(customerService.getCustomerCouponsByMaxPrice(maxPrice), HttpStatus.ACCEPTED);
-    }
+    }*/
 
+    @GetMapping("myCouponsByCategory/{categories}")
+    public ResponseEntity<?> getCustomerCouponsByCategory(@RequestHeader(name = "Authorization") String token, @PathVariable("categories") Categories categories) throws MalformedJwtException, CustomerUserException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(customerService.getCustomerCouponsByCategory(categories));
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     /**
      * This verb enable the customer to get all coupons by category from the system from external server.
      * @param categories of the coupon
      * @return ResponseEntity<> with list of all coupons and HTTP status.
      * @throws CustomerUserException
-     */
+     *
     @GetMapping("myCouponsByCategory/{categories}")
     private ResponseEntity<?>getCustomerCouponsByCategory(@PathVariable("categories") Categories categories) throws CustomerUserException {
         try {
@@ -78,14 +116,22 @@ public class CustomerController {
         } catch (CustomerUserException e) {
             throw new CustomerUserException(e.getMessage());
         }
-    }
+    }*/
 
+    @GetMapping("/getMyCoupons")
+    public ResponseEntity<?> getMyCoupons(@RequestHeader(name = "Authorization") String token) throws MalformedJwtException, CustomerUserException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(customerService.getCustomerCoupons());
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     /**
      * This verb enable the customer to get all coupons that were purchased from external server.
      *
      * @return ResponseEntity<> with list of all purchased coupons and HTTP status.
      * @throws CustomerUserException
-     */
+     *
     @GetMapping("/getMyCoupons")
     private ResponseEntity<?>getMyCoupons() throws CustomerUserException {
         try {
@@ -93,17 +139,25 @@ public class CustomerController {
         } catch (CustomerUserException e) {
             throw new CustomerUserException(e.getMessage());
         }
-    }
+    }*/
 
+    @GetMapping("/CustomerDetails")
+    public ResponseEntity<?> getCustomerDetails(@RequestHeader(name = "Authorization") String token) throws MalformedJwtException {
+        if (jwtUtil.validateToken(token)) {
+            return ResponseEntity.ok().headers(getHeaders(token)).body(customerService.getCustomerDetails());
+        }
+        return new ResponseEntity<>(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+    /*
     /**
      * This verb enable the customer to get all customer details from external server.
      *
      * @return ResponseEntity<> with list of all customer details and HTTP status.
      *
-     */
+     *
     @GetMapping("/CustomerDetails")
     private ResponseEntity<?>getCustomerDetails(){
         return new ResponseEntity<>(customerService.getCustomerDetails(),HttpStatus.OK);
-    }
+    }*/
 
 }
